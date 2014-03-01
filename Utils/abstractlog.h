65767cedae2540d619b1b08fcab94e1833d94e33
Nicola Pezzotti <nicola.pezzotti@gmail.com>
2014-03-01 11:03:39 +0100

    Adds some utility macros for the log handling

--- Utils/abstractlog.h
+++ Utils/abstractlog.h
@@ -2,6 +2,7 @@
 #define ABSTRACTLOG_H
 
 #include <string>
+#include <sstream>
 
 class AbstractLog{
 public:
@@ -9,5 +10,14 @@ public:
     virtual void clear()=0;
     virtual void display(const std::string& d)=0;
 };
+
+#define SECURE_LOG(log,text){if((log)!=nullptr){(log)->display(text);}}
+#define SECURE_LOG_VAL(log,text,v){if((log)!=nullptr){\
+                                        std::stringstream ss;\
+                                        ss << text << ": " << v;\
+                                        (log)->display(ss.str());\
+                                    }}
+#define SECURE_LOG_VERBOSE(log,text){if(_verbose){SECURE_LOG(log,text)}}
+#define SECURE_LOG_VAL_VERBOSE(log,text,v){if(_verbose){SECURE_LOG_VAL(log,text,v)}}
 
 #endif // ABSTRACTLOG_H
